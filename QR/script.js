fa7ec4ae0b231108ac4b8dd27de9ad12462319e0
apala7778 <thetrialbyexistence@gmail.com>
2023-03-22 11:51:54 +0530

    first commit

---
+++ QR/script.js
@@ -0,0 +1,22 @@
+
+
+let img = document.createElement("img");
+let qrr = document.querySelector(".qr-code");
+
+ qrr.appendChild(img);
+  function generate(){
+    let input = document.getElementById("input");
+    if (input.value) {
+        input.style.borderColor = "#c7c7c7";
+        img.src = `https://api.qrserver.com/v1/create-qr-code/?size=180x180&data=${input.value}`;
+    } 
+    else {
+        input.style.borderColor = "red";
+        return false;
+
+
+    }
+     input.value= " ";
+    }
+  
+  
